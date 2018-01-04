f409fa26fa8f7aaca28ecbd4e2cec28bd751db04
Mark Griffith <mgriffit@ualberta.ca>
2018-01-04 14:01:41 -0700

    first_fit algorithm complete. needs testing

--- Assignment5/bin.py
+++ Assignment5/bin.py
@@ -1,21 +1,29 @@
-## bin.py - defines the Bin class to be used by the bin packing algorithm
+# Mark Griffith
 
-class Bin:
+
+class Bin(object):
+    """ Bin class to be used with the Bin-Packing alg """
     def __init__(self):
         self.bin_list = []
         self.sum = 0
 
-    def show_bin(self):
+    def get_bin_list(self):
+        """shows the bin list"""
         return self.bin_list
 
     def add(self, obj):
+        """appends object to the bin list"""
         self.bin_list.append(obj)
+        self.sum += obj
 
     def remove(self, obj):
+        """removes object from the bin list"""
         self.bin_list.remove(obj)
+        self.sum -= obj
 
     def get_sum(self):
-        for i in self.bin_list:
-            self.sum += i
+        """returns the sum of the objects
+           in the bin list"""
+        # for i in self.bin_list:
+        #     self.sum += i
         return self.sum
-
--- Assignment5/pack.py
+++ Assignment5/pack.py
@@ -1,11 +1,38 @@
+########################################
+# Mark Griffith
+#
+# Implementation of Bin-Packing algorithm
+# using a First-Fit strategy
+#
+########################################
+
 from bin import Bin
 
-if __name__ == "__main__":
-    b = Bin()
-    b.add(1)
-    b.add(2)
-    b.remove(1)
-    print(b.get_sum())
-    print b.show_bin()
 
-    print "yellow"
+def first_fit(obj_list, size_max):
+    """ Creates and returns a list of the
+        bins populated with the given objects """
+
+    bin_list = [Bin()]
+
+    for object in obj_list:
+        alloc_new_bin = True
+
+        # scan through list of bins until one is found with
+        # room to fit the object
+        for bin in bin_list:
+            if bin.get_sum() + object <= size_max:
+                bin.add(object)
+                alloc_new_bin = False
+                break
+
+        # if object cannot be added to an existing bin
+        # create a new bin for the object to be put in
+        if alloc_new_bin is True:
+            newbin = Bin()
+            newbin.add(object)
+            bin_list.append(newbin)
+
+    # print items in bins as lists
+    for bin in bin_list:
+        print bin.get_bin_list()
