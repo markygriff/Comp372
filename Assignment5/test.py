32221e87118ecf5ae52f8df505be30761a77a482
Mark Griffith <mgriffit@ualberta.ca>
2018-01-07 15:00:49 -0700

    minor update to test.py

--- Assignment5/test.py
+++ Assignment5/test.py
@@ -4,5 +4,4 @@ from first_fit_decreasing import first_fit_decreasing
 if __name__ == "__main__":
     objs = [0.5, 0.4, 0.35, 0.9, 0.7, 0.3, 0.1, 0.1, 0.5]
     first_fit(objs)
-
     first_fit_decreasing(objs)
