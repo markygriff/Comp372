93308702e5c6e817ad04fff80c1f51245eebcccb
Mark Griffith <mgriffit@ualberta.ca>
2018-01-09 11:23:41 -0700

    added comments and cleaned it up

--- Assignment5/random_test.sh
+++ Assignment5/random_test.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+#
+# Mark Griffith
+#
+# this script automates the testing of both the first-fit
+# and first-fit-decreasing heuristic using random input data
+#
 
 [ -f random_input.txt ] && rm random_input.txt
 touch random_input.txt
 
 while true; do
-  read -p "Would you like to run decreasing? " yn
+  read -p "Would you like to run decreasing? (y/n) " yn
   case $yn in
     [Yy]* ) alg="first_fit_decreasing.py"; break;;
     [Nn]* ) alg="first_fit.py"; break;;
@@ -14,14 +20,15 @@ done
 
 cnt=0
 objects=0
-
-while [ $cnt -le 30 ]; do
-  while [ $objects -le 499 ]; do
+while [ $cnt -le 30 ]; do # 30 iterations by default
+  while [ $objects -le 499 ]; do # 500 objects by default
+    # append object sizes to the input file
     n=$(python -c "import random; print random.uniform(0.01,0.99)")
     echo $n >> random_input.txt
     ((objects++))
   done
   objects=0
+  # run the chosen algorithm on the current input data
   python $alg random_input.txt
   ((cnt++))
 done
--- Assignment5/worst_case_test.sh
+++ Assignment5/worst_case_test.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+#
+# Mark Griffith
+#
+# this script automates the testing of both the first-fit
+# and first-fit-decreasing heuristic using the worst case input data
+#
 
 [ -f worst_input.txt ] && rm worst_input.txt
 touch worst_input.txt
 
 while true; do
-  read -p "Would you like to run decreasing? " yn
+  read -p "Would you like to run decreasing? (y/n) " yn
   case $yn in
     [Yy]* ) alg="first_fit_decreasing.py"; break;;
     [Nn]* ) alg="first_fit.py"; break;;
@@ -14,12 +20,14 @@ done
 
 cnt=0
 objects=0
-while [ $cnt -le 30 ]; do
-  while [ $objects -le 499 ]; do
+while [ $cnt -le 30 ]; do # 30 iterations by default
+  while [ $objects -le 499 ]; do  # 500 objects by default
+    # append object sizes to the input file
     echo 0.9 >> worst_input.txt
     ((objects++))
   done
   objects=0
+  # run the chosen algorithm on the current input data
   python $alg worst_input.txt
   ((cnt++))
 done
