f8e6b7b1dfed7ec460a20a2d8be2b628910448c0
Mark Griffith <mgriffit@ualberta.ca>
2018-01-09 11:21:37 -0700

    added comments and timing function

--- Assignment5/first_fit.py
+++ Assignment5/first_fit.py
@@ -1,18 +1,20 @@
 # Mark Griffith
 #
-# Implementation of Bin-Packing algorithm
-# using a First-Fit strategy
+# Implementation of Bin-Packing algorithm using a First-Fit heuristic
 #
 
 import argparse
-import timeit
+import time
 from bin import Bin
 
 
 def first_fit(obj_list):
-    """ Creates and returns a list of the
-        bins populated with the given objects """
+    """ Creates a list of bins required to fit the
+    given list of objects.
 
+    Returns the number of bins used. """
+
+    # initialize the list of bin objects
     bin_list = [Bin()]
 
     for object in obj_list:
@@ -22,37 +24,51 @@ def first_fit(obj_list):
         # room to fit the object
         for bin in bin_list:
             if bin.get_sum() + object <= 1:
+                # allocate object size to the found bin
                 bin.add(object)
                 alloc_new_bin = False
                 break
 
-        # if object cannot be added to an existing bin
+        # if the object cannot be added to an existing bin
         # create a new bin for the object to be put in
         if alloc_new_bin is True:
             newbin = Bin()
             newbin.add(object)
             bin_list.append(newbin)
 
-    # print items in bins as lists
-    # for num, bin in enumerate(bin_list):
-        # print "Bin", num, ":", bin.get_bin_list()
-    print "Total bins used =", len(bin_list)
+    return len(bin_list)
+
+
+
+def time_fn( fn, *args, **kwargs ):
+    """ Times the given function.
+
+    Returns the elapsed time, and function returns. """
+
+    start = time.clock()
+    results = fn( *args, **kwargs )
+    end = time.clock()
+    return end-start, results
 
 
 if __name__ == "__main__":
+    # parse arguments
     pars = argparse.ArgumentParser()
     pars.add_argument("obj_file", help="path to file containing objects")
     args = pars.parse_args()
 
+    # read the file data as a list of object sizes
     with open(args.obj_file) as f:
-        lines = f.read().split()
+        objects = f.read().split()
 
-    lines = map(float, lines)
-    t = timeit.Timer(lambda: first_fit(lines))
-    elapsed = t.timeit(number=1)
+    # call the first-fit algorithm and time its execution
+    objects = map(float, objects)
+    elapsed, num_bins = time_fn(first_fit, objects)
 
+    # append first-fit results to the output file
     with open('plotting/plot_data.txt', 'a') as f:
-        print >> f, len(lines), elapsed
+        print >> f, len(objects), elapsed
 
-    print "Total number of objects =", len(lines)
+    print "Total number of objects =", len(objects)
+    print "Total number of bins used =", num_bins
     print "Total elapsed time =", elapsed, "seconds"
--- Assignment5/first_fit_decreasing.py
+++ Assignment5/first_fit_decreasing.py
@@ -1,17 +1,35 @@
 # Mark Griffith
+#
+# Implementation of Bin-Packing algorithm using a First-Fit-Decreasing
+# heuristic
+#
 
 import argparse
-import timeit
+import time
 from first_fit import first_fit
 
 
 def first_fit_decreasing(obj_list):
     """ Sorts the object list in decreasing order
     and calls the first_fit algorithm using the
-    sorted list """
+    sorted list.
+
+    Returns the number of bins used. """
 
     obj_list.sort(reverse=True)
-    first_fit(obj_list)
+    num_bins = first_fit(obj_list)
+    return num_bins
+
+
+def time_fn( fn, *args, **kwargs ):
+    """ Times the given function.
+
+    Returns the elapsed time, and function returns. """
+
+    start = time.clock()
+    results = fn( *args, **kwargs )
+    end = time.clock()
+    return end-start, results
 
 
 if __name__ == "__main__":
@@ -19,15 +37,18 @@ if __name__ == "__main__":
     pars.add_argument("obj_file", help="path to file containing objects")
     args = pars.parse_args()
 
+    # read the file data as a list of object sizes
     with open(args.obj_file) as f:
-        lines = f.read().split()
+        objects = f.read().split()
 
-    lines = map(float, lines)
-    t = timeit.Timer(lambda: first_fit_decreasing(lines))
-    elapsed = t.timeit(number=1)
+    # call the first-fit-decreasing algorithm and time its execution
+    objects = map(float, objects)
+    elapsed, num_bins = time_fn(first_fit_decreasing, objects)
 
+    # append first-fit-decreasing results to the output file
     with open('plotting/plot_data.txt', 'a') as f:
-        print >> f, len(lines), elapsed
+        print >> f, len(objects), elapsed
 
-    print "Total number of objects =", len(lines)
+    print "Total number of objects =", len(objects)
+    print "Total number of bins used =", num_bins
     print "total elapsed time =", elapsed, "seconds"
